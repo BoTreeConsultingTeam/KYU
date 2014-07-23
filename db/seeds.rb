94f12927d9d57d7fb60e8c3856419381bd9ac098
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-23 15:27:37 +0530

    #4 - Create roles model and database table and Add roles in table through seed file.
    WIP :- Add emails and default passwords for users table trough seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def create_or_update_by_alias(class_name, row)
+  instance  = class_name.find_by_role_alias(row[:role_alias])
+  if instance.blank?
+    class_name.create!(row)
+    puts "#{row[:name]}  added."
+  else
+    instance.update_attributes(row)
+    puts "#{row[:name]} updated."
+  end
+  instance
+end
+
+# Roles
+roles = [ { name: 'Operator', role_alias: Role::OPERATOR },
+          { name: 'Administrator', role_alias: Role::ADMINISTRATOR },
+          { name: 'Teacher', role_alias: Role::TEACHER },
+          { name: 'Student', role_alias: Role::STUDENT } ]
+
+puts '------------Seeding Roles------------'
+roles.each { |role| create_or_update_by_alias(Role, role)}
+puts '-------------------------------------'
