61d1189656e73703a57bc5b9085356bd32b34d9e
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-23 15:41:21 +0530

    #4 - Add default users email and password using seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,23 @@ roles = [ { name: 'Operator', role_alias: Role::OPERATOR },
 puts '------------Seeding Roles------------'
 roles.each { |role| create_or_update_by_alias(Role, role)}
 puts '-------------------------------------'
+
+
+#================================================Create Default Users===================================================
+
+def find_or_create_user(user_attrs)
+  email = user_attrs[:email]
+  user = User.find_by_email(email)
+
+  if user.nil?
+    user = User.create(user_attrs)
+    puts "Created user having email #{email}"
+  else
+    puts "User having email #{email} already exists, thus not created"
+  end
+  user
+end
+
+operator = find_or_create_user({ email: 'operator@kyu.com', password: Settings.default_password })
+admin = find_or_create_user({ email: 'admin@kyu.com', password: Settings.default_password })
+teacher = find_or_create_user({ email: 'teacher@kyu.com', password: Settings.default_password })
