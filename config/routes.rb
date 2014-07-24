2e02d96c7dd1788db91c3b643f2d76b043de1a11
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-24 15:27:54 +0530

    #11 - Apply Boostrap 3 CSS In Sign In and Forgot Password Page 1) Apply Boostrap 3 CSS In Sign In and Forgot Password Pages. 2) Complete flow of functionality for Sign In and Forgot Password Pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 KYU::Application.routes.draw do
   root to: 'static_pages#index'
-  devise_for :users
+
+  devise_for :users do
+    get '/signin' => 'devise/sessions#new'
+    get '/signout' => 'devise/sessions#destroy'
+  end
 end
