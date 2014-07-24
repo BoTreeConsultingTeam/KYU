5d50bd13cef69f715bc2561b7100e07c514e99ec
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-24 10:13:42 +0530

    #4 - Create Seed File for 1) Create roles model and database table. 2) Add roles in table through seed file. 3) Add emails and default passwords for users table trough seed file.

--- db/schema.rb
+++ db/schema.rb
@@ -21,18 +21,25 @@ ActiveRecord::Schema.define(version: 20140723093849) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "salutation"
+    t.string   "highest_qualification"
+    t.string   "first_name"
+    t.string   "middle_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.boolean  "active",                 default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
