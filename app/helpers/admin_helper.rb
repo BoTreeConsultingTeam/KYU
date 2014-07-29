f92801f54a9954eba4ea09379b2145ea0a3d0b36
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-07-29 11:52:22 +0530

    #13 - Add helper for display roles dropdown

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,10 @@
 module AdminHelper
+  def roles_dropdown(roles, selected=nil)
+    # The title attribute value is used to load on Add/Edit Operator/Teacher
+    options = roles.collect do |role|
+                [role.name, role.id]
+              end
+
+    select_tag("user_role[role_id]", options_for_select(options, selected), prompt: t('registration.caption.role_select_prompt'), class: 'form-control')
+  end
 end
