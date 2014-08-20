04f83fee3dc8e1bc9adbae043cdc15b373c365f0
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-08-20 10:58:57 +0530

    Remove role, user, user_role models

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :user_roles
-  has_many :roles, through: :user_roles
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
