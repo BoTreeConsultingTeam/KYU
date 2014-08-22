5da77d6ef161b1269477fe0cb240720e93a20c8b
HirenBhalani-BTC <h.o.bhalani@gmail.com>
2014-08-22 18:21:40 +0530

    # 31 owner of question can select the right answer and selected answer is visible to all user. added one column to answer table for check selected answer - DONE

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,17 @@ class Question < ActiveRecord::Base
 	has_many :users, through: :answers, dependent: :destroy
 
 	accepts_nested_attributes_for :answers
+
+	def answered?
+		answers.where(flag: true).count > 0
+	end
+
+	def ans_id
+		answers.each do |ans|
+			if ans.flag== true
+				logger.debug "-------------------------#{ans.flag}"
+				return ans.id
+			end
+		end
+	end
 end
