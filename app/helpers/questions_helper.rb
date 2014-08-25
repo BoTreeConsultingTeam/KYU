baca276828e14098c4348a7e52f7d18220fa336f
NishantUpadhyay-BTC <nishantupadhyay26@gmail.com>
2014-08-25 14:13:39 +0530

    #21 - add helper method to set link and a pagination partial also set to render anywhere

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def get_user_name(id)
+    Student.find(Question.find(id)).first_name
+  end
+
+  def set_link(title,time)
+  	link_to title, questions_path(:time => 'week')
+  end
 end
