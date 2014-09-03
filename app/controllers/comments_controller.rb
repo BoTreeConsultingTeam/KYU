9773cc2cf380a6e8ec96bb4ffb0e0d3b88a94b62
NishantUpadhyay-BTC <nishantupadhyay26@gmail.com>
2014-09-03 11:19:39 +0530

    #44 - add badges-display page in application - add points and badges functionality with theme integration

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,101 @@
+class CommentsController < ApplicationController
+
+  before_action :user_signed_in?
+  
+  def create 
+    relative_of_comment = params[:relative]
+    @comment = Comment.create(comment_params.merge({commentable: current_user}).merge({relative: relative_of_comment}))
+    @answer = Answer.find_by_id(params[:comment][:relative_id])
+    if "Question" == relative_type
+      redirect_to question_path(relative_id)
+    else
+      redirect_to question_path(@answer.question.id)
+    end
+  end
+   
+  def update
+    @comment=Comment.find_by_id(params[:id])
+    @comment.update(comment_params)
+    if !(@comment.nil?)
+      @comment.update(comment_params)
+      if "Question" == relative_type
+        redirect_to question_path(params[:comment][:relative_id]),flash: { success: "Updated Successfully" }
+      else
+        @answer = Answer.find_by_id(params[:comment][:relative_id])
+        redirect_to question_path(@answer.question),flash: { success: "Updated Successfully" }
+      end
+    else
+      if "Question" == relative_type
+        redirect_to question_path(relative_id),flash: { error: "no such Comment found for Update" }
+      else
+        @answer = Answer.find_by_id(params[:comment][:relative_id])
+        redirect_to question_path(@answer.question),flash: { error: "no such Comment found for Update" }
+      end
+    end
+  end
+
+  def show
+    @answer = Answer.find_by_id(params[:id])
+    if @answer.nil?
+      redirect_to question_path(params[:question_id]),flash: { error: "no such Answer found for Comment" }
+    else
+      @comment = Comment.new
+      @comments = Comment.relative_comments(@answer.id,@answer.class)
+    end
+  end
+  
+  def edit
+    if params[:question_id]
+      @question = Question.find_by_id(params[:question_id])
+      @answers = @question.answers
+      @comment = Comment.find_by_id(params[:id])
+      if @comment.nil?
+        redirect_to question_path(params[:question_id]),flash: { error: "No such Comment found for Edit!" }
+      else
+        @comments_q = Comment.relative_comments(@question.id,@question.class)
+      end
+    else
+      @answer = Answer.find_by_id(params[:answer_id])
+      @comment = Comment.find_by_id(params[:id])
+      if @comment.nil?
+        redirect_to comment_path(params[:answer_id]),flash: { error: "No such Comment found for Edit!" }
+      else       
+        @comments = Comment.relative_comments(@answer.id,@answer.class)
+      end
+    end     
+  end    
+
+  def destroy
+    @comment = Comment.find_by_id(params[:id])
+    if !(@comment.nil?)
+      @comment.delete
+      if params[:answer_id].present?
+        @answer = Answer.find_by_id(params[:answer_id])
+        redirect_to question_path(@answer.question.id),flash: { success: "Deleted Successfully!" }
+      else
+        redirect_to question_path(params[:question_id]),flash: { success: "Deleted Successfully!" }
+      end
+    else
+      if params[:answer_id].present?
+        @answer = Answer.find_by_id(params[:answer_id])
+        redirect_to question_path(@answer.question.id),flash: { error: "No such Comment found for Delete!" }
+      else
+        redirect_to question_path(params[:question_id]),flash: { error: "No such Comment found for Delete!" }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+      params.require(:comment).permit!
+  end
+
+  def relative_id
+    params[:comment][:relative_id]
+  end
+
+  def relative_type
+    params[:comment][:relative_type]
+  end
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,16 @@
+class Comment < ActiveRecord::Base
+  
+  include Findable
+  include ActsAsCommentable::Comment
+  belongs_to :question
+  belongs_to :answer
+  belongs_to :relative, :polymorphic => true
+  belongs_to :commentable, :polymorphic => true
+  scope :relative_comments, ->(relative_id,relative_class) {where("relative_id = ? AND relative_type = ?",relative_id,relative_class)}
+  scope :all_comments_of_answers, -> (relative_type) {where("relative_type = ?",relative_type)}
+  validates :comment, presence: true
+
+  def is_author?(user)
+   self.commentable == user
+  end
+end
