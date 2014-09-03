f322d1745f9170aaf5a7630305362d1095be2dbd
bhushanuj <bhushan.jadhav@beaverlogic.com>
2014-09-03 15:50:23 +0530

    change schema to latest version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827105851) do
+ActiveRecord::Schema.define(version: 20140902083100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,16 +27,13 @@ ActiveRecord::Schema.define(version: 20140827105851) do
     t.boolean  "flag",            default: false
   end
 
-  create_table "badges_sashes", force: true do |t|
-    t.integer  "badge_id"
-    t.integer  "sash_id"
-    t.boolean  "notified_user", default: false
+  create_table "badges", force: true do |t|
+    t.string   "name"
+    t.integer  "points"
+    t.boolean  "default"
     t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
-  add_index "badges_sashes", ["badge_id", "sash_id"], name: "index_badges_sashes_on_badge_id_and_sash_id", using: :btree
-  add_index "badges_sashes", ["badge_id"], name: "index_badges_sashes_on_badge_id", using: :btree
-  add_index "badges_sashes", ["sash_id"], name: "index_badges_sashes_on_sash_id", using: :btree
 
   create_table "comments", force: true do |t|
     t.string   "title",            limit: 50
@@ -80,36 +77,11 @@ ActiveRecord::Schema.define(version: 20140827105851) do
   add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", using: :btree
   add_index "impressions", ["user_id"], name: "index_impressions_on_user_id", using: :btree
 
-  create_table "merit_actions", force: true do |t|
-    t.integer  "user_id"
-    t.string   "action_method"
-    t.integer  "action_value"
-    t.boolean  "had_errors",    default: false
-    t.string   "target_model"
-    t.integer  "target_id"
-    t.boolean  "processed",     default: false
+  create_table "levels", force: true do |t|
+    t.integer  "badge_id"
+    t.integer  "student_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "merit_activity_logs", force: true do |t|
-    t.integer  "action_id"
-    t.string   "related_change_type"
-    t.integer  "related_change_id"
-    t.string   "description"
-    t.datetime "created_at"
-  end
-
-  create_table "merit_score_points", force: true do |t|
-    t.integer  "score_id"
-    t.integer  "num_points", default: 0
-    t.string   "log"
-    t.datetime "created_at"
-  end
-
-  create_table "merit_scores", force: true do |t|
-    t.integer "sash_id"
-    t.string  "category", default: "default"
   end
 
   create_table "questions", force: true do |t|
@@ -120,11 +92,6 @@ ActiveRecord::Schema.define(version: 20140827105851) do
     t.datetime "updated_at"
     t.integer  "askable_id"
     t.string   "askable_type"
-  end
-
-  create_table "sashes", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "students", force: true do |t|
@@ -145,8 +112,7 @@ ActiveRecord::Schema.define(version: 20140827105851) do
     t.string   "last_name"
     t.string   "username"
     t.date     "birthdate"
-    t.integer  "sash_id"
-    t.integer  "level",                  default: 0
+    t.integer  "points"
   end
 
   add_index "students", ["email"], name: "index_students_on_email", unique: true, using: :btree
