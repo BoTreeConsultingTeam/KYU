6ac8eb0cc95b62270fc66b443bf34c1f942a1624
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-09-26 13:23:49 +0530

    Changed port number

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ server "104.131.111.55", :web, :app, :db, primary: true
 
 set :application, "kyu"
 set :user, "kyu"
-set :port, 1026
+set :port, 2345
 set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
